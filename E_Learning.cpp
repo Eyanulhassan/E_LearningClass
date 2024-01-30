c86572c6c857f3e7c6bf82316527dc4f6931640b
unknown <Administrator@UCPCLB203-57.ucp.com>
2024-01-30 12:15:19 +0500

    E-learning file added

---
+++ E_Learning.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+class ElearningSystem {
+private:
+  
+  String name;
+string username;
+
+public:
+    
+    ElearningSystem() {
+      
+    }
+
+   
+bool authenticateUser();
+bool Live();
+bool Course();
+bool Grades();
+bool Enrolement();
+bool fourm();
+bool assessments();
+
+
+        
+};
+
+int main() {
+    ElearningSystem elearningSystem;
+
+
+    return 0;
+}
